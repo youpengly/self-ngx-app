26097166333a0dcc4b8b8acf42e02902367d3f66
youpeng <youpenglymx@gmail.com>
2018-04-17 11:24:18 +0800

    production build cancel console

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  // production build cancel all console statement
+  window.console.log = (...args) => {};
+  window.console.warn = (...args) => {};
+  window.console.error = (...args) => {};
+  window.console.debug = (...args) => {};
   enableProdMode();
 }
 
